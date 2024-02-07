8217d81191c2470a2f75490d777918a9fed9db98
Gebruiker47 <t.orhan.h@hotmail.com>
2024-02-07 08:35:36 +0100

    Project opzet
    Volgens getekende applicatie structuur zijn alle benodigde mappen en bestanden toegevoegd.

---
+++ src/imports/imports.js
@@ -0,0 +1,24 @@
+import "../index.css";
+
+import "../components/add-crypto";
+import "../components/my-select-option";
+
+import "../components/dashboard/my-assets";
+import "../components/dashboard/my-chart";
+import "../components/dashboard/my-dashboard";
+
+import "../components/inputs/custom-button";
+import "../components/inputs/custom-text-input";
+
+import "../components/users/user-login";
+import "../components/users/user-profile";
+import "../components/users/user-registration";
+
+import "../router/router";
+
+import "../ui/footer-navbar-menu";
+import "../ui/my-container";
+import "../ui/my-header";
+import "../ui/my-modal";
+import "../ui/my-router-menu";
+import "../ui/top-navbar-menu";
